dbd892b09e67b94a5aaf93fe8ebf26c5e5016398
midzi354 <79577108+midzi354@users.noreply.github.com>
2023-05-26 10:02:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ require 'system/config/koneksi.php'
       <h1 align="center">Lokasi Bank Sampah</h1>
       <br>
       <br>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810.8982007168704!2d119.45481452912792!3d-5.1633916139043885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee2e61806efa3%3A0x871e1040a9f97a8a!2sBank%20Sampah%20Pusat%20Kota%20Makassar!5e1!3m2!1sid!2sid!4v1685066186988!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810.8982007168704!2d119.45481452912792!3d-5.1633916139043885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee2e61806efa3%3A0x871e1040a9f97a8a!2sBank%20Sampah%20Pusat%20Kota%20Makassar!5e1!3m2!1sid!2sid!4v1685066186988!5m2!1sid!2sid" width="1362" height="470" frameborder="0" style="margin-bottom: 0; left: 10px;" allowfullscreen></iframe>
     </div>
   </div>
 
